31e7865b330dacdfaad10bbd91e7023038fd1eb3
miladkhajavi <mkhajavi5@gmail.com>
2019-10-01 18:14:08 +0330

    added logger

--- tools/adminController.js
+++ tools/adminController.js
@@ -73,7 +73,126 @@ module.exports = {
       })
     }
 
+  },
+
+
+
+
+  ////////////////////////////blocking user ///////////////////////
+
+  async blocking(req, res) {
+    try {
+
+
+      const filter = req.body._id;
+      const update = true
+      const user = await User.findByIdAndUpdate(filter, {
+        block: true
+      })
+
+
+      if (!user) return res.json({
+        success: false,
+        msg: 'customer blocked not fond'
+      });
+      res.json({
+        success: true,
+        user,
+        msg: 'Successful customer blocked.'
+      });
+
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`
+      })
+
+    }
+  },
+  /////////////////////un blocking user  //////////////////
+  async unblocking(req, res) {
+    try {
+
+
+      const filter = req.body._id;
+      const update = true
+      const user = await User.findByIdAndUpdate(filter, {
+        block: false
+      })
+
+
+      if (!user) return res.json({
+        success: false,
+        msg: 'customer blocked not fond'
+      });
+      res.json({
+        success: true,
+        user,
+        msg: 'Successful customer unblocked.'
+      });
+
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`
+      })
+
+    }
+  },
+
+  ////////////////////////show  all blocked user/////////////////////////
+  /////////
+
+  async blockedUser(req, res) {
+    try {
+      const findBlock = await User.find({
+        block: true
+      })
+      if (!findBlock) return res.json({
+        success: false,
+        msg: 'customer findBlock not fond'
+      });
+      res.json({
+        success: true,
+        findBlock,
+        msg: 'Successful All findBlock  Customer.'
+      });
+
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`
+      })
+
+    }
+  },
+  //////////////////////////////show unblock users ///////////////////
+  async unblockUser(req, res) {
+    try {
+      const findUnblockBlock = await User.find({
+        block: false
+      })
+      if (!findUnblockBlock) return res.json({
+        success: false,
+        msg: 'customer unblock not fond'
+      });
+      res.json({
+        success: true,
+        findUnblockBlock,
+        msg: 'Successful All unBlock  Customer.'
+      });
+
+    } catch (error) {
+      res.status(500).send({
+        error: `An error has occured ${error}`
+      })
+
+    }
   }
+
+
+
+
+
+
+
 
 
 }
